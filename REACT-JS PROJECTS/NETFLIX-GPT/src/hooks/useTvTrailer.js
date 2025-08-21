2f9318315c12edd4cfbb7d82f425411a9de9d197
kapilsarkar <k.sarkar81@gmail.com>
2025-08-21 08:57:13 +0530

    updated

--- REACT-JS PROJECTS/NETFLIX-GPT/src/hooks/useTvTrailer.js
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/hooks/useTvTrailer.js
@@ -14,7 +14,7 @@ const useTvTrailer = (tvId) => {
         API_OPTIONS
       );
       const json = await data.json();
-      console.log(json?.results);
+      //console.log(json?.results);
 
       const filterData = json.results.filter((video) => video?.type === "Trailer");
       const trailer = filterData.length ? filterData[0] : json.results[0];
