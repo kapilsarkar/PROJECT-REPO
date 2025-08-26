86e9d6925ee9262330b94f5512cb199851b8399c
kapilsarkar <k.sarkar81@gmail.com>
2025-08-26 16:48:00 +0530

    somepartsadded

--- REACT-JS PROJECTS/NETFLIX-GPT/src/components/GptMovieSuggestions.jsx
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/components/GptMovieSuggestions.jsx
@@ -1,22 +1,26 @@
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
+import Footer from "./Footer";
 
 const GptMovieSuggestions = () => {
   const { movieResults, gptMovieNames } = useSelector((store) => store.gpt);
   if (!gptMovieNames) return null;
 
   return (
-    <div className="p-6 m-4 bg-gradient-to-b from-gray-900 to-black text-white bg-opacity-100 rounded-2xl shadow-lg">
-      <div className="w-full space-y-8">
-        {gptMovieNames.map((movieName, index) => (
-          <MovieList
-            key={movieName}
-            title={movieName}
-            movies={movieResults[index]}
-          />
-        ))}
+    <>
+      <div className="p-6 m-4 bg-gradient-to-b from-gray-900 to-black text-white bg-opacity-100 rounded-2xl shadow-lg">
+        <div className="w-full space-y-8">
+          {gptMovieNames.map((movieName, index) => (
+            <MovieList
+              key={movieName}
+              title={movieName}
+              movies={movieResults[index]}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
