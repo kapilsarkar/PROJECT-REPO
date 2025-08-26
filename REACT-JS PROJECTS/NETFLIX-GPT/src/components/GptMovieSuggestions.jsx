7cd6ab7bf20096497794f4296b2ce07573e25d9b
kapilsarkar <k.sarkar81@gmail.com>
2025-08-26 14:15:52 +0530

    soomepartsadded

--- REACT-JS PROJECTS/NETFLIX-GPT/src/components/GptMovieSuggestions.jsx
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/components/GptMovieSuggestions.jsx
@@ -1,5 +1,23 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
 const GptMovieSuggestions = () => {
-  return <div>GptMovieSuggestions</div>;
+  const { movieResults, gptMovieNames } = useSelector((store) => store.gpt);
+  if (!gptMovieNames) return null;
+
+  return (
+    <div className="p-6 m-4 bg-gradient-to-b from-gray-900 to-black text-white bg-opacity-100 rounded-2xl shadow-lg">
+      <div className="w-full space-y-8">
+        {gptMovieNames.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={movieResults[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GptMovieSuggestions;
--- REACT-JS PROJECTS/NETFLIX-GPT/src/utils/constant.js
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/utils/constant.js
@@ -1,13 +1,17 @@
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTA2ZjA0YzA0NzNlM2VkM2NhOWQ3ZmQ2OTVlNmRjOCIsIm5iZiI6MTcwNTIxNTI5Ny4wMDcsInN1YiI6IjY1YTM4NTQwMzk1NDlhMDEzMjEwYWFkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3S2IIYOuBg9396ZVlXmHxIhL3WyS8_f0R6tK7DVpImI'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + import.meta.env.VITE_TMDB_KEY,
+  },
 };
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-export const SUPPORTED_LANGUAGES = [{ identifier: "en", name: "English" }, { identifier: "hindi", name: "Hindi" }, { identifier: "spanish", name: "Spanish" }];
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
 
 export const OPENAI_KEY = import.meta.env.VITE_GEMINI_API_KEY;
