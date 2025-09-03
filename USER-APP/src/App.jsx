e31a8e3f0e0deda65f1229fdcc881cf94e5076e1
kapilsarkar <k.sarkar81@gmail.com>
2025-09-03 07:45:18 +0530

    somepartsadded

--- USER-APP/src/App.jsx
+++ USER-APP/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <h2 className=" text-xl ">User-App</h2>
+      <h2 className=" text-xl ">User-Management-App</h2>
       <Header/>
     </>
   );
