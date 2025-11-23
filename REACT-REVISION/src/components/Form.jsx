b5191d0ff3df2bc9c047007b51075c765e3555f0
kapilsarkar <k.sarkar81@gmail.com>
2025-11-23 08:38:43 +0530

    somepartsadded

--- REACT-REVISION/src/components/Form.jsx
+++ REACT-REVISION/src/components/Form.jsx
@@ -1,9 +1,27 @@
+import { useRef } from "react";
+
 const Form = () => {
+  const inputRef = useRef(null);
+  const inputHandler = () => {
+    console.log(inputRef);
+    inputRef.current.focus();
+    inputRef.current.style.color = "red";
+    inputRef.current.placeholder = "Enter Password";
+  };
+  const toggleHandler = () => {
+    if (inputRef.current.style.display != "none") {
+      inputRef.current.style.display = "none";
+    } else {
+      inputRef.current.style.display = "inline";
+    }
+  };
   return (
     <>
       <div>
         <h2>Use Ref hook</h2>
-        <input type="text" placeholder="Enter..." />
+        <input ref={inputRef} type="text" placeholder="Enter..." />
+        <button onClick={inputHandler}>Focus On Input Field</button>
+        <button onClick={toggleHandler}>Toggle</button>
       </div>
     </>
   );
