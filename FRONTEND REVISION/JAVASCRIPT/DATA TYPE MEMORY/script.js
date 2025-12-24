314aaaa37b8bea30e0591e1cf39150e7da02572b
kapilsarkar <k.sarkar81@gmail.com>
2025-12-24 07:47:16 +0530

    somepartsadded

--- FRONTEND REVISION/JAVASCRIPT/DATA TYPE MEMORY/script.js
+++ FRONTEND REVISION/JAVASCRIPT/DATA TYPE MEMORY/script.js
@@ -19,3 +19,16 @@ let b = a;
 b= 20;
 console.log(a);
 console.log(b);
+
+//Data  Type conversion
+//1. Number
+let num = 123
+let value = Number(num);
+console.log(num);
+console.log(typeof value);
+
+//2. String
+let str = 123
+let strValue = String(str);
+console.log(str);
+console.log(typeof strValue);
