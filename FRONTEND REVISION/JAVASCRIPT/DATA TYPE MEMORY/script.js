55c4eb7896181a5a2eef0df0bbbface28157ea61
kapilsarkar <k.sarkar81@gmail.com>
2025-12-25 08:40:48 +0530

    somepartsadded

--- FRONTEND REVISION/JAVASCRIPT/DATA TYPE MEMORY/script.js
+++ FRONTEND REVISION/JAVASCRIPT/DATA TYPE MEMORY/script.js
@@ -3,8 +3,8 @@
 //Heap Memory Example
 
 let person1 = {
-    name:"Kapil",
-    age:34,
+    name: "Kapil",
+    age: 34,
 }
 
 let person2 = person1;
@@ -16,7 +16,7 @@ console.log(person2.name);
 //Stack Memory example
 let a = 10;
 let b = a;
-b= 20;
+b = 20;
 console.log(a);
 console.log(b);
 
@@ -32,3 +32,49 @@ let str = 123
 let strValue = String(str);
 console.log(str);
 console.log(typeof strValue);
+
+//Logical Operators
+let x = true;
+let y = false;
+console.log("Logical AND", x && y);
+console.log("Logical OR", x || y);
+console.log("Logical Not", !x);
+console.log("Logical Not", !y);
+
+//Practical Example of Logical Operators
+let age = 34;
+let userId = false;
+if (age > 18 && userId) {
+    console.log("You are eligible to vote");
+}
+else {
+    console.log("You are Not eligible to Vote");
+}
+
+//For-in Loop
+let person = {
+    name: "Kapil",
+    age: 34,
+    city: "Kolkata",
+};
+
+for(let key in person){
+    console.log(key + ": " +person[key]);
+}
+
+//for of Loop
+let colors = ["Red", "Green", "Blue", "Yellow"];
+for(let color of colors){
+    console.log(color);
+}
+
+//Real example
+const students =[
+    {name: "Kapil", age: 34},
+    {name: "Piku", age: 30},
+    {name: "Brock", age: 28},   
+]
+
+for(let student of students){
+    console.log(student.name + ": " + student.age);
+}
